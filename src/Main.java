ab5d5bb7c07d5081f7223a854ed56d777d978652
Kauê Fernandes <90159661+fernandes-kaue@users.noreply.github.com>
2024-07-24 16:05:47 -0300

    exercicio h pagina 101

--- src/Main.java
+++ src/Main.java
@@ -2,57 +2,45 @@ public class Main {
 
     public static void main(String[] args) {
 
-        /* matriz a 7x7
-        * matriz b = valor do elemento correspondente em a + seus indices
-        * exceto quando os indices da diagonal principal forem impares, neste caso, b = a!
-        * */
+        int[][] numsA = new int[10][7];
+        double totalOdds = 0, finalOddRate = 0, totalEvens = 0, finalEvenRate = 0;
 
-        int[][] vetorA = new int[7][7];
-        int[][] vetorB = new int[7][7];
-
-        // Populating vetorA
-        for (int i = 0; i < vetorA.length; i++) {
-            for (int j = 0; j < vetorA[i].length; j++) {
-                vetorA[i][j] = i + j + 1;
+        for (int i = 0; i < numsA.length; i++) {
+            for (int j = 0; j < numsA[i].length; j++) {
+                numsA[i][j] = i*(j+1);
             }
         }
 
-        // Populating vetorB
-        for (int i = 0; i < vetorB.length; i++) {
-            for (int j = 0; j < vetorB[i].length; j++) {
-                if (i == j && i % 2 != 0) {
-                    vetorB[i][j] = (int) factorial(vetorA[i][j]); // Set factorial for diagonal
-                } else {
-                    vetorB[i][j] = vetorA[i][j] + i + j; // Regular case
-                }
-            }
-        }
-
-        // Print vetorA and vetorB side by side
-        System.out.println("Matrix A and Matrix B:");
-        for (int i = 0; i < vetorA.length; i++) {
-            for (int j = 0; j < vetorA[i].length; j++) {
-                System.out.print(vetorA[i][j] + " ");
-            }
-            System.out.print("   "); // Spacer between matrices
-            for (int j = 0; j < vetorB[i].length; j++) {
-                System.out.print(vetorB[i][j] + " ");
+        for (int[] nums : numsA) {
+            for (int num : nums) {
+                System.out.print(num + " ");
             }
             System.out.println();
         }
 
+        int totalElements = numsA.length * numsA[0].length;
+
+        for (int[] ints : numsA) {
+            for (int anInt : ints) {
+                if (anInt % 2 == 0) {
+                    totalOdds++;
+                } else {
+                    totalEvens++;
+                }
+                finalOddRate = (totalOdds / totalElements) * 100;
+                finalEvenRate = (totalEvens / totalElements) * 100;
+            }
+        }
+
+        System.out.println();
+
+        System.out.println("Total of Odd numbers: " + totalOdds + "\nTotal of Even Numbers: " + totalEvens + "\n");
+
+        System.out.println("Final Odd Rate: " + finalOddRate + "\nFinal Even Rate: " + finalEvenRate);
+
+
     }
 
-    // Method to calculate factorial
-    public static long factorial(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException("Factorial is not defined for negative numbers");
-        }
-        long result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-        return result;
-    }
+
 
 }
