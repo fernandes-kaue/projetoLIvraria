50145654ec2d5bc292bf12e48ec7035699e311d2
Kauê Fernandes <90159661+fernandes-kaue@users.noreply.github.com>
2024-08-20 19:22:55 -0300

    Finished Library Project

---
+++ src/Library/Book.java
@@ -0,0 +1,19 @@
+package Library;
+
+/**
+ * @param title attributes
+ */
+public record Book(String title, String author, String ISBN) {
+    // constructor
+
+    // specific methods
+    @Override
+    public String toString() {
+        return title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
